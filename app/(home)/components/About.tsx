6482a142f7f6f869e8d2b51d0a0347597ef52682
Tanish Singhal <tanishsinghal510@gmail.com>
2024-06-03 00:03:56 +0530

    slight change in the layout

--- app/(home)/components/About.tsx
+++ app/(home)/components/About.tsx
@@ -11,39 +11,26 @@ function About() {
         className="flex flex-col items-center justify-center -rotate-6 animate-move-down"
       />
 
-      <div className="flex flex-wrap pt-12">
-        <div className="w-full lg:w-1/3 lg:p-2">
-          <div className="flex justify-center items-center">
-            <Image
-              className="rounded-2xl h-auto w-72"
-              src={aboutImg}
-              alt="personImage"
-            />
-          </div>
-        </div>
-        <div className="w-full pt-10 lg:w-2/3 lg:pt-0">
-          <div className="flex justify-center lg:justify-start">
-            <p className="my-2 max-w-2/3 text-xl text-gray-300">
-              I'm Tanish Singhal, a B.Tech student and aspiring <b>Full-Stack
-              Developer</b> with a passion for innovation. My expertise spans both
-              front-end and back-end development, allowing me to tackle projects
-              from start to finish. I'm particularly fascinated by <b>Blockchain
-              Technology</b> and its potential to reshape the web.
-              <br />
-              <br />
-              A strong believer in continuous learning, I actively contribute to
-              <b> Open Source Projects</b> and build personal ventures to sharpen my
-              skills. These projects showcase my ability to craft user-centric
-              interfaces and develop robust back-end solutions, all while
-              staying at the forefront of technological advancements.
-              <br />
-              <br />
-              <b>Ready to Collaborate?</b> I'm eager to bring my enthusiasm and
-              expertise to your project. Let's build something groundbreaking
-              together!
-            </p>
-          </div>
-        </div>
+      <div className="flex justify-center flex-wrap w-full pt-10 mx-auto ">
+        <p className="my-2 text-xl text-gray-300 text-center">
+          I'm Tanish Singhal, a B.Tech student and aspiring{" "}
+          <b>Full-Stack Developer</b> with a passion for innovation. My
+          expertise spans both front-end and back-end development, allowing me
+          to tackle projects from start to finish. I'm particularly fascinated
+          by <b>Blockchain Technology</b> and its potential to reshape the web.
+          <br />
+          <br />A strong believer in continuous learning, I actively contribute
+          to
+          <b> Open Source Projects</b> and build personal ventures to sharpen my
+          skills. These projects showcase my ability to craft user-centric
+          interfaces and develop robust back-end solutions, all while staying at
+          the forefront of technological advancements.
+          <br />
+          <br />
+          <b>Ready to Collaborate?</b> I'm eager to bring my enthusiasm and
+          expertise to your project. Let's build something groundbreaking
+          together!
+        </p>
       </div>
     </section>
   );
--- app/(home)/components/HeroSection.tsx
+++ app/(home)/components/HeroSection.tsx
@@ -4,6 +4,8 @@ import React from "react";
 import Link from "next/link";
 import { MovingBorderBtn } from "@/components/ui/moving-border";
 import Title from "./Title";
+import Image from "next/image";
+import aboutImg from "/public/Profile Photo.png";
 
 function HeroSection() {
   function scrollToContact() {
@@ -14,59 +16,50 @@ function HeroSection() {
   }
 
   return (
-    <div className="min-h-[60vh] flex flex-col-reverse gap-20 lg:gap-0 lg:flex-row items-center justify-between mt-16 md:mt-0 animate-move-up">
-      <div className="space-y-10 text-center  lg:text-left">
-        <h1 className="text-4xl lg:text-7xl font-bold">
-          Nice to meet you!👋🏻
-          <br />{" "}
+    <div className="min-h-[60vh] flex flex-col-reverse gap-20 lg:gap-0 md:flex-row items-center justify-between mt-16 md:mt-0 animate-move-up">
+      <div className="space-y-10 text-center md:text-left">
+        <h1 className="text-4xl md:text-6xl font-bold">
+          Nice to meet you!
+          <br />
           <span className="underline underline-offset-8 decoration-green-500">
-            {"I'm Tanish."}
+            I'm Tanish.
           </span>
         </h1>
 
-        <div className="flex justify-center lg:justify-start">
-          <p className="text-center text-gray-300 w-96  lg:text-left">
-            Based in India. I'm a Full Stack Developer passionate about building a modern Web Application that users love.
-          </p>
-        </div>
+        <p className="text-xl text-gray-300 w-96 mx-auto md:mx-0">
+          Based in India. I'm a Full Stack Developer passionate about building
+          modern Web Applications that users love.
+        </p>
 
-        <div className="flex items-center mt-4 lg:mt-8">
-          <Link href="" className="inline-block mr-16" onClick={scrollToContact}>
+        <div className="flex items-center justify-center md:justify-start mt-4 lg:mt-8">
+          <Link href="" onClick={scrollToContact}>
             <Title text="Contact Me 📬" />
           </Link>
-
-          <a href="/Tanish-Resume.pdf" download>
+          <a href="/Tanish-Resume.pdf" download className="ml-4">
             <MovingBorderBtn
               borderRadius="2rem"
               className="text-md p-3 font-semibold"
             >
-              <p>Download Resume 📄</p>
+              Download Resume 📄
             </MovingBorderBtn>
           </a>
         </div>
       </div>
 
-      <div className="relative md:mt-10">
-        <div className="w-72 h-72 space-y-3 -rotate-[30deg] relative">
-          <div className="flex gap-3 translate-x-8">
-            <div className="w-32 h-32 rounded-2xl bg-green-500"></div>
-            <div className="w-32 h-32 rounded-full bg-indigo-500"></div>
-          </div>
-
-          <div className="flex gap-3 -translate-x-8">
-            <div className="w-32 h-32 rounded-2xl bg-indigo-500"></div>
-            <div className="w-32 h-32 rounded-full bg-green-500"></div>
-          </div>
-
-          <div className="glow absolute top-[40%] right-1/2 -z-10"></div>
-        </div>
-
-        <div className="absolute bottom-5 sm:bottom-14 left-0 sm:-left-10">
+      <div className="relative md:mt-8">
+        <Image
+          className="rounded-2xl h-auto w-72"
+          src={aboutImg}
+          alt="personImage"
+        />
+        <div className="absolute bottom-6 left-7">
           <MovingBorderBtn
             borderRadius="0.5rem"
             className="text-md p-2 font-semibold"
           >
-            <Link href="mailto:tanishsinghal510@gmail.com">📢 Available for Work</Link>
+            <Link href="mailto:tanishsinghal510@gmail.com">
+              📢 Available for Work
+            </Link>
           </MovingBorderBtn>
         </div>
       </div>
